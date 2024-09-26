a138cdf5585675c929f1a3b8df5d0443d1227841
rutviksorathia <sorathiarutvik2000@gmail.com>
2024-09-27 00:58:15 +0530

    fix: ui

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:smooth_corner/smooth_corner.dart';
 import 'package:stacked/stacked.dart';
 import 'package:store_transform_task/services/connectivity_service.dart';
 import 'package:store_transform_task/ui/views/no_internet/no_internet_view.dart';
@@ -21,6 +23,9 @@ class HomeView extends StackedView<HomeViewModel> {
     HomeViewModel viewModel,
     Widget? child,
   ) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarBrightness: Brightness.dark),
+    );
     final connectivityController = Get.find<ConnectivityController>();
     return Obx(
       () {
@@ -29,69 +34,206 @@ class HomeView extends StackedView<HomeViewModel> {
         }
 
         return Scaffold(
-          backgroundColor: Theme.of(context).colorScheme.surface,
-          body: Container(
-            padding: const EdgeInsets.only(left: 25.0, right: 25.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                if (viewModel.selectedScreenShow == ScreenShow.login)
-                  TextField(
-                    controller: viewModel.employeeCodeController,
-                  )
-                else if (viewModel.selectedScreenShow == ScreenShow.break_in)
-                  Column(
+          body: Stack(
+            children: [
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: 0,
+                top: 0,
+                child: Container(
+                  width: double.infinity,
+                  color: Colors.black,
+                ),
+              ),
+              Positioned(
+                left: -60,
+                top: -60,
+                child: Container(
+                  height: 150,
+                  width: 150,
+                  decoration: ShapeDecoration(
+                    shape: SmoothRectangleBorder(
+                      borderRadius: BorderRadius.circular(65),
+                    ),
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+              Positioned(
+                right: -60,
+                bottom: -60,
+                child: Container(
+                  height: 150,
+                  width: 150,
+                  decoration: ShapeDecoration(
+                    shape: SmoothRectangleBorder(
+                      borderRadius: BorderRadius.circular(65),
+                    ),
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    left: 18,
+                    right: 18,
+                    top: MediaQuery.of(context).padding.top + 47,
+                    bottom: MediaQuery.of(context).padding.bottom,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      RadioListTile(
-                        value: true,
-                        title: const Text(
-                          "Long Break (In)",
+                      Center(
+                        child: Container(
+                          height: 200,
+                          width: 200,
+                          decoration: ShapeDecoration(
+                            shape: SmoothRectangleBorder(
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                            color: Colors.grey.shade900,
+                          ),
+                          child: Icon(
+                            Icons.camera_alt_outlined,
+                            size: 60,
+                            color: Colors.grey.shade800,
+                          ),
                         ),
-                        groupValue: viewModel.selectedBreakType == BreakType.lb,
-                        onChanged: (val) {
-                          viewModel.selectedBreakType = BreakType.lb;
-                          viewModel.notifyListeners();
-                        },
                       ),
-                      RadioListTile(
-                        value: true,
-                        title: const Text(
-                          "Short Break (In)",
-                        ),
-                        groupValue: viewModel.selectedBreakType == BreakType.sb,
-                        onChanged: (val) {
-                          viewModel.selectedBreakType = BreakType.sb;
-                          viewModel.notifyListeners();
-                        },
+                      const SizedBox(height: 20),
+                      Icon(
+                        Icons.abc_rounded,
+                        size: 60,
+                        color: Colors.grey.shade800,
                       ),
-                      RadioListTile(
-                        value: true,
-                        title: const Text(
-                          "Logout",
-                        ),
-                        groupValue:
-                            viewModel.selectedBreakType == BreakType.logout,
-                        onChanged: (val) {
-                          viewModel.selectedBreakType = BreakType.logout;
-                          viewModel.notifyListeners();
-                        },
-                      ),
+                      const SizedBox(height: 20),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          if (viewModel.selectedScreenShow == ScreenShow.login)
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  "Employee Code",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                const SizedBox(height: 12),
+                                TextFormField(
+                                  controller: viewModel.employeeCodeController,
+                                  decoration: InputDecoration(
+                                    labelText: "006",
+                                    border: const OutlineInputBorder(),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                        color: Colors.deepPurple.shade800,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            )
+                          else if (viewModel.selectedScreenShow ==
+                              ScreenShow.break_in)
+                            Column(
+                              children: [
+                                RadioListTile(
+                                  value: true,
+                                  title: const Text(
+                                    "Long Break (In)",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  groupValue: viewModel.selectedBreakType ==
+                                      BreakType.lb,
+                                  onChanged: (val) {
+                                    viewModel.selectedBreakType = BreakType.lb;
+                                    viewModel.notifyListeners();
+                                  },
+                                ),
+                                RadioListTile(
+                                  value: true,
+                                  title: const Text(
+                                    "Short Break (In)",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  groupValue: viewModel.selectedBreakType ==
+                                      BreakType.sb,
+                                  onChanged: (val) {
+                                    viewModel.selectedBreakType = BreakType.sb;
+                                    viewModel.notifyListeners();
+                                  },
+                                ),
+                                RadioListTile(
+                                  value: true,
+                                  title: const Text(
+                                    "Logout",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  groupValue: viewModel.selectedBreakType ==
+                                      BreakType.logout,
+                                  onChanged: (val) {
+                                    viewModel.selectedBreakType =
+                                        BreakType.logout;
+                                    viewModel.notifyListeners();
+                                  },
+                                ),
+                              ],
+                            )
+                          else
+                            Text(
+                              viewModel.selectedBreakType == BreakType.lb
+                                  ? "Long Break (Out)"
+                                  : "Short Break (Out)",
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white,
+                              ),
+                            ),
+                          const SizedBox(height: 20),
+                          InkWell(
+                            onTap: viewModel.handleSubmitButtonTap,
+                            child: Container(
+                              color: Colors.red,
+                              child: const Padding(
+                                padding: EdgeInsets.all(12.0),
+                                child: Text(
+                                  "Submit",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
                     ],
-                  )
-                else
-                  Text(
-                    viewModel.selectedBreakType == BreakType.lb
-                        ? "Long Break (Out)"
-                        : "Short Break (Out)",
                   ),
-                ElevatedButton(
-                  onPressed: viewModel.handleSubmitButtonTap,
-                  child: const Text(
-                    "Submit",
-                  ),
-                )
-              ],
-            ),
+                ),
+              )
+            ],
           ),
         );
       },
